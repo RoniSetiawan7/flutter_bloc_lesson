2ba72d081f9ced49c49ad06800fc43a04dc6dec7
ronisetiawan7 <ronisetiawan0109@gmail.com>
2022-12-09 08:21:08 +0700

    add bool isAuth to hive box for check auth status

--- lib/login/services/login_service.dart
+++ lib/login/services/login_service.dart
@@ -5,6 +5,7 @@ import 'package:hive/hive.dart';
 class LoginService {
   late Box<Login> _login;
   late Box<Register> _register;
+  late Box _box;
 
   Future<void> init() async {
     if (!Hive.isAdapterRegistered(0) && !Hive.isAdapterRegistered(1)) {
@@ -14,12 +15,16 @@ class LoginService {
 
     _login = await Hive.openBox('login_box');
     _register = await Hive.openBox('register_box');
+    _box = await Hive.openBox('box');
   }
 
   Future<LoginEnum> authenticateUser(String email, String password) async {
     final success = _register.values.any(
         (element) => element.email == email && element.password == password);
-    await _login.add(Login(email, password));
+    if (success) {
+      await _login.add(Login(email, password));
+      await _box.put('isAuth', true);
+    }
 
     LoginEnum result = success ? LoginEnum.registered : LoginEnum.notRegistered;
     return result;
